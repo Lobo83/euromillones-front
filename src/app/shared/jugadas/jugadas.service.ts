986dbc44530f4750bb8b95c4b1bc28f72eea208e
lobo83 <inmortalland83@gmail.com>
2021-12-19 08:43:27 +0100

    añadido documentacion a jugada service y metodo de obtención de medidas frecuentes

--- src/app/shared/jugadas/jugadas.service.ts
+++ src/app/shared/jugadas/jugadas.service.ts
@@ -4,16 +4,62 @@ import { EMPTY, Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { JugadaVO } from './jugadavo';
 
+/**
+ *
+ *
+ * @export
+ * @class JugadasService
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class JugadasService {
 
+  /**
+   * Creates an instance of JugadasService.
+   * @param {HttpClient} http
+   * @memberof JugadasService
+   */
   constructor(private http: HttpClient) { }
 
+  /**
+   *
+   *
+   * @param {number} numeroJugadas
+   * @return {*}  {Observable<JugadaVO[]>}
+   * @memberof JugadasService
+   */
   obtenerJugadasAleatorias(numeroJugadas:number):Observable<JugadaVO[]>{
     let params = new HttpParams().set('numeroJugadas', numeroJugadas);
-    return this.http.get<JugadaVO[]>("http://localhost:8080/jugada/aleatoria?numeroJugadas=1").pipe(catchError(err => {
+    return this.http.get<JugadaVO[]>("http://localhost:8080/jugada/aleatoria", { params }).pipe(catchError(err => {
+
+      console.log(`Error ${err} invocando a backend`);
+
+      return EMPTY; //Observable.empty YA NO EXISTE
+      //Lanznado EMPTY no se entra por la gestion de error pero no se desconecta del observable
+      //Si lanzamos throwError(err) entonces entrará por la gestion de error del subscribe padre pero se desregistra todos los observables
+    }));
+  }
+
+  /**
+   *
+   *
+   * @param {number} longitud
+   * @param {Date} fechaInicial
+   * @param {Date} fechaFinal
+   * @param {number} frecuenciaMinima
+   * @param {number} numeroJugadas
+   * @return {*}  {Observable<JugadaVO[]>}
+   * @memberof JugadasService
+   */
+  obtenerJugadasFrequentes(longitud: number, fechaInicial: Date, fechaFinal: Date, frecuenciaMinima: number, numeroJugadas: number): Observable<JugadaVO[]> {
+    let params = new HttpParams().set('numeroJugadas', numeroJugadas).
+    set('longitud',longitud)
+    .set('fechaInicial',fechaInicial.toDateString())
+    .set('fechaFinal',fechaFinal.toDateString())
+    .set('numeroJugadas',numeroJugadas);
+
+    return this.http.get<JugadaVO[]>("http://localhost:8080/jugada/frecuente",{params}).pipe(catchError(err => {
       
         console.log(`Error ${err} invocando a backend`);
       
